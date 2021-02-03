13f7a8a1e7b62bbc85c21428241dfac2cfd9ce13
putn1k <skachok11@yandex.ru>
2021-02-03 21:09:47 +0300

    переделал слайдер, добавил scroll up

--- source/js/main.js
+++ source/js/main.js
@@ -11,6 +11,14 @@ $(function () {
     $('html,body').stop().animate({ scrollTop: $('#main_content').offset().top }, 1000);
     e.preventDefault();
   });
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('.scroll-up').fadeIn();
+    } else {
+      $('.scroll-up').fadeOut();
+    }
+  });
 });
 
 $(function () {
@@ -26,13 +34,34 @@ $(function () {
 });
 
 //slider
-let carousel_1;
-let carousel_2;
-if (window.matchMedia('(max-width: 767px)').matches) {
-  $(document).ready(function () {
-    carousel_1 = $(".documents__licenses");
-    carousel_2 = $(".partners__list");
-    carousel_1.itemslide();
-    carousel_2.itemslide();
-  });
-}
+
+const slider = document.querySelector('.swiper-container');
+const slider1 = document.querySelector('.swiper-container1');
+
+let mySwiper = new Swiper(slider, {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: false,
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+    },
+    320: {
+      slidesPerView: 1,
+    }
+  }
+});
+
+let mySwiper1 = new Swiper(slider1, {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: false,
+  autoplay: {
+    delay: 5000,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 5,
+    }
+  }
+})
